3b7379e2b2b5575786a564e2b1839df91843c4cc
Gaurav Harkhani <gauravharkhani01@gmail.com>
2025-04-22 14:04:59 +0530

    first commit

---
+++ Crypto Tracker/Crypto_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_TrackerApp.swift
+//  Crypto Tracker
+//
+//  Created by Gaurav Harkhani on 22/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
